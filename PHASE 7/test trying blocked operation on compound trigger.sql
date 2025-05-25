12837c72fd686f96ea5bb88dc2b04d87f3130be9
IYUKURI NUGI YABESI <nugiybes@gmail.com>
2025-05-25 05:32:49 +0200

    Add files via upload

---
+++ PHASE 7/test trying blocked operation on compound trigger.sql
@@ -0,0 +1,5 @@
+---test: trying blocked operation on compound trigger-----
+
+
+INSERT INTO Transaction (TransactionID, AccountID, MerchantID, TransactionAmount, TransactionDate)
+VALUES (3011, 1, 2, 49.99, SYSDATE);
