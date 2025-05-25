5c42983f18023d1bb87524fa4f8198ac80fa19d2
IYUKURI NUGI YABESI <nugiybes@gmail.com>
2025-05-25 06:07:07 +0200

    Add files via upload

---
+++ PHASE 6/DML INSERT.sql
@@ -0,0 +1,12 @@
+----DML: Insert------
+
+
+
+INSERT INTO Transaction_Analysis (AnalysisID, UserID, TotalSpent, AvgTransactionAmount, HighestSpendingMerchant, AnalysisDate)
+VALUES (1, 1, 1000.00, 250.00, 'ElectroMart', SYSDATE);
+
+INSERT INTO Transaction_Analysis (AnalysisID, UserID, TotalSpent, AvgTransactionAmount, HighestSpendingMerchant, AnalysisDate)
+VALUES (2, 2, 800.00, 200.00, 'FoodZone', SYSDATE);
+
+INSERT INTO Transaction_Analysis (AnalysisID, UserID, TotalSpent, AvgTransactionAmount, HighestSpendingMerchant, AnalysisDate) 
+VALUES (3, 3, 1500.00, 300.00, 'BookHub', SYSDATE);
